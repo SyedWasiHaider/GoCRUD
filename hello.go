cd04f1fbfc12ed5d410eb89098430ee9cf71d9d7
WasiDeveloper <syedwasihaider@github.com>
2015-03-02 00:12:20 -0500

    Added an update method

--- hello.go
+++ hello.go
@@ -3,6 +3,7 @@ package main
 import (
 "fmt"
 "bytes"
+"strconv"
 "log"
 "encoding/json"
 "github.com/jinzhu/gorm"
@@ -13,9 +14,9 @@ _ "github.com/go-sql-driver/mysql"
 
 type ProductListing struct {
 	ID         int  `sql:"not null;unique;auto_increment;primary key"`
-    Name     string
-    Description      string
-    Price float64
+	Name     string
+	Description      string
+	Price float64
 }
 
 var db gorm.DB;
@@ -25,6 +26,7 @@ func main(){
 	db = setupDB();
 	r := mux.NewRouter()
 	r.HandleFunc("/Create", create).Methods("POST");
+	r.HandleFunc("/Update/{id}", update).Methods("PUT");
 	r.HandleFunc("/Find/{name}", getListing).Methods("GET");
 	http.Handle("/", r)
 	http.ListenAndServe(":1234", nil)
@@ -54,23 +56,54 @@ func setupDB()  (gorm.DB) {
 }
 
 /**************CRUD OPERATIONS*************/
-
+//Some repetitive code here that can be refactored...
 
 func create(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	log.Println(r.Body)
-    var t ProductListing;   
-    err := decoder.Decode(&t)
-    if err != nil {
-        http.Error(w, "Could not decode JSON", 400);
-    }else{
+	var t ProductListing;   
+	err := decoder.Decode(&t)
+	if err != nil {
+		http.Error(w, "Could not decode JSON", 400);
+	}else{
     	//The response writer automagically sends 200 as the
     	//response code.
-		fmt.Fprintf(w, "All Good")
 		addProductListing(&t);
+		fmt.Fprintf(w, "All Good")
 
-    }
-    log.Println(t.Name)
+	}
+	log.Println(t.Name)
+}
+
+func update(w http.ResponseWriter, r *http.Request){
+	//Gets the id parameter
+	params := mux.Vars(r);
+	idStr := params["id"]
+	id, errParse := strconv.Atoi(idStr)
+    if errParse != nil {
+        // handle error
+        http.Error(w, "Not a valid ID", 400);
+	}
+
+
+	decoder := json.NewDecoder(r.Body)
+	log.Println(r.Body)
+	var t ProductListing;   
+	err := decoder.Decode(&t)
+	if err != nil {
+		http.Error(w, "Could not decode JSON", 400);
+	}else{
+
+		listing := getProductListingById(id);
+		if (listing!=nil && listing.ID > 0){
+			t.ID = listing.ID;
+			db.Where("id=?", id).Save(&t);
+			fmt.Fprintf(w, "All Good")
+		}else{
+			http.Error(w, "No listing with that id exists", 400);
+		}
+	}
+
 }
 
 
@@ -88,33 +121,40 @@ func getListing(w http.ResponseWriter, r *http.Request){
 		Coverts the array of listings to a json array
 		and returns it.
 	*/
-	var buffer bytes.Buffer;
-	jsonBytes, err := json.Marshal(listings)
-    
-    if err != nil {
-    		   http.Error(w, "Couldn't encode JSON", 500);
-    }
+		var buffer bytes.Buffer;
+		jsonBytes, err := json.Marshal(listings)
 
-	buffer.WriteString(string(jsonBytes));
-	fmt.Fprintf(w, "%s", buffer.String());
-}
+		if err != nil {
+			http.Error(w, "Couldn't encode JSON", 500);
+		}
+
+		buffer.WriteString(string(jsonBytes));
+		fmt.Fprintf(w, "%s", buffer.String());
+	}
 
 
 /**************DB OPERATIONS***************/
 
-func addProductListing(listing * ProductListing){
+	func addProductListing(listing * ProductListing){
 
 	//To ensure the ID is set by the DB and not the user.
-	listing.ID = 0; 
-	db.NewRecord(&listing)
-	db.Create(&listing)
-}
+		listing.ID = 0; 
+		db.NewRecord(&listing)
+		db.Create(&listing)
+	}
 
-func getProductListingByName(name string) ([] ProductListing){
+	func getProductListingById(id int) (*ProductListing){
 	//Find all items matching the name
-	var listings [] ProductListing;
-	db.Where("name = ?", name).Find(&listings)
-	return listings;
-}
+		var listing ProductListing;
+		db.Where("id = ?", id).Find(&listing)
+		return &listing;
+	}
+
+	func getProductListingByName(name string) ([] ProductListing){
+	//Find all items matching the name
+		var listings [] ProductListing;
+		db.Where("name = ?", name).Find(&listings)
+		return listings;
+	}
 
 
