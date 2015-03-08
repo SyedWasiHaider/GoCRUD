cdf8bbcf9a892904b7912f8449034f72dc8f90ed
WasiDeveloper <syedwasihaider@github.com>
2015-03-07 21:48:07 -0500

    Added delete and findbyid method

--- src/crud/routes.go
+++ src/crud/routes.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-"github.com/gorilla/mux"
-"net/http"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
-func setupRoutesAndServe(){
+func setupRoutesAndServe() {
 	r := mux.NewRouter()
-	r.HandleFunc("/Create", create).Methods("POST");
-	r.HandleFunc("/Update/{id}", update).Methods("PUT");
-	r.HandleFunc("/Find/{name}", getListing).Methods("GET");
+	r.HandleFunc("/Create", create).Methods("POST")
+	r.HandleFunc("/Update/{id}", update).Methods("PUT")
+	r.HandleFunc("/FindByName/{name}", getListing).Methods("GET")
+	r.HandleFunc("/Find/{id}", getListingById).Methods("GET")
+	r.HandleFunc("/Delete/{id}", deleteListing).Methods("DELETE")
 	http.Handle("/", r)
 	http.ListenAndServe(":1234", nil)
 }
